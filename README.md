b30d0569e1606e26ce412df410298e7702f4f5bd
RasakCodes <rasaksalaama@gmail.com>
2024-06-20 10:35:28 +0100

    updated

---
+++ README.md
@@ -0,0 +1,5 @@
+This is an "Airbnb" website.
+
+It aims at capturing the beauty, and business opportunities inherent in Airbnb project.
+
+
---
+++ helpers/date.js
@@ -0,0 +1,19 @@
+// import {format} from "date-fns";
+//
+// export const formattedDate = (startDate, endDate) => {
+//     const formattedStartDate = format(new Date(startDate), "dd MMMM yyyy");
+//     const formattedEndDate = format(new Date(endDate), "dd MMMM yyyy");
+//
+//     const range = `${formattedStartDate} - ${formattedEndDate}`;
+//
+//     return {formattedEndDate, formattedStartDate, range}
+// }
+//
+//
+//
+let data = {
+    signIn: false.valueOf,
+    signOut: verify,
+};
+
+export default data;
